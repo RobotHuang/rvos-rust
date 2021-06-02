d859c8b19cd7749786952cfa4491e9c9ca830e16
RobotHuang <1183598761@qq.com>
2021-06-02 18:11:47 +0800

    00-bootstrap
    Signed-off-by: RobotHuang <1183598761@qq.com>

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+
+#![no_main]
+
+#![feature(global_asm)]
+
+use core::panic::PanicInfo;
+
+global_asm!(include_str!("start.S"));
+
+/// 当 panic 发生时会调用该函数
+/// 我们暂时将它的实现为一个死循环
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn start_kernel() -> ! {
+    loop {}
 }
